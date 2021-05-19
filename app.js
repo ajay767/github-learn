79cee009a303304fa289fcde023ef817b9eb5b72
Ajay yadav <ajayyadav@pop-os.localdomain>
2021-05-19 07:52:13 +0530

    added new file -helper

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
 console.log('my function is making billion dollar in an hour!!');
-
-function add(...arr) {
-  let sum = 0;
-  arr.forEach((e) => (sum += e));
-  return sum;
-}
---
+++ helper.js
@@ -0,0 +1,5 @@
+function add(...arr) {
+  let sum = 0;
+  arr.forEach((e) => (sum += e));
+  return sum;
+}
