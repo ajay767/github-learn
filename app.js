efe4bfba7742a292863b3076c5103d704d24afd2
Ajay yadav <ajayyadav@pop-os.localdomain>
2021-05-19 07:50:44 +0530

    added school payment handler methods

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log('my function is making billion dollar in an hour!!');
+
+class Payment {
+  constructor(receipentDepartment, amountPaid) {
+    this.receipentDepartment = receipentDepartment;
+    this.amountPaid = amountPaid;
+  }
+}
+
+class Student {
+  constructor(name, birthYear, standard) {
+    this.name = name;
+    this.birthYear = birthYear;
+    this.standard = standard;
+    this.feePaid = 0;
+    this.paymentHistory = [];
+  }
+
+  payFee(amount, paidTo) {
+    const receipt = new Payment(amount, paidTo);
+    this.paymentHistory.push(receipt);
+    this.feePaid += amount;
+  }
+}
