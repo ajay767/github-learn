9aded789971e14cf6ad236a70e206ff3f6c64b9c
Ajay yadav <ajayyadav@pop-os.localdomain>
2021-05-19 09:27:11 +0530

    added teacher appointer

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log('my function is making billion dollar in an hour!!'); //this is my conflict arrising commit
 
+//this is payment method handler
 class Payment {
   constructor(receipentDepartment, amountPaid) {
     this.receipentDepartment = receipentDepartment;
@@ -7,6 +8,7 @@ class Payment {
   }
 }
 
+//this is new student admission handler
 class Student {
   constructor(name, birthYear, standard) {
     this.name = name;
@@ -20,5 +22,14 @@ class Student {
     const receipt = new Payment(amount, paidTo);
     this.paymentHistory.push(receipt);
     this.feePaid += amount;
+  }
+}
+
+//this is new teacher apointer
+class Teacher {
+  constructor(name, department, experience) {
+    this.name = name;
+    this.department = department;
+    this.experience = experience;
   }
 }
